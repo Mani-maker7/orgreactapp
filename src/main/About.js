f37635dfe5aa160060b0419febda80385cce5e1c
Manikanta Vemuri <manikantavemuri77@gmail.com>
2024-05-01 11:09:51 +0530

    error fixes

--- src/main/About.js
+++ src/main/About.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-        <h3>I am in about page</h3>
+    <div> <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <h3>The Student Course Management System (SCMS) is an integrated software solution designed to streamline and optimize the administrative processes associated with managing courses, students, and educational resources within academic institutions. At its core, the SCMS serves as a centralized platform that facilitates tasks such as course scheduling, enrollment management, attendance tracking, grading, and communication between stakeholders. Through intuitive interfaces and robust features, students can easily browse course offerings, register for classes, and access course materials, while instructors can efficiently manage course content, track student progress, and provide feedback. Administrators benefit from comprehensive tools for managing academic programs, generating reports, and analyzing data to support decision-making. By leveraging technology to automate routine tasks, improve communication, and enhance data accuracy, the SCMS empowers academic institutions to optimize efficiency, foster student success, and enhance the overall teaching and learning experience.</h3>
     </div>
   )
 }
